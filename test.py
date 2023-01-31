1d8ee81fb35a04aed9c31fb1dd42f62a8edff942
Joey <joseph.schmidt@utexas.edu>
2023-01-31 14:16:31 -0600

    Added even and odd opeations to test.py file

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 # Comment thing
 
-a = 2
-b = 3
-c = a + 2*b
+q = 3
+n = 4
 
-print(c)
+def odd_operation(n, q):
+    odd_step = int(q*n + 1)
+    return odd_step
+
+def even_operation(n):
+    even_step = int(n/2)
+    return even_step
+
+def odd_even_operation(n, q):
+    if (n%2 == 0): #divisible by 2, or even
+        N = even_operation(n)
+    else:
+        N = odd_operation(n,q)
+    return N
+
+
+N = odd_even_operation(n, q)
+print(n,">", N)
+
