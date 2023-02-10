4fc1baa159b3ab61266947ed4103f02b90412dc0
Joey <joseph.schmidt@utexas.edu>
2023-02-09 23:28:40 -0600

    Turned file into function for use elsewhere

--- rm_dict_builder.py
+++ rm_dict_builder.py
@@ -2,51 +2,50 @@ from statistics import mode
 from collections import defaultdict
 from odd_to_odd_func import odd_to_odd_step_counter
 
-q = 5
-N = 30001
 
-#Setup odds to extract r,m info from
-odd_range = range(1, N, 2)
+def rm_dict_builder(q, n_max):
 
-#Loop through odds and collect the r (steps) for each odd
-step_dict = defaultdict(list)
-for n in odd_range:
-    odd_0, odd_1, steps = odd_to_odd_step_counter(n,q)
-    step_dict[steps].append(odd_0)
+    #Setup odds to extract r,m info from
+    odd_range = range(1, n_max, 2)
 
-step_dict = dict(sorted(step_dict.items())) #sort r (step) dict numerically
+    #Loop through odds and collect the r (steps) for each odd
+    step_dict = defaultdict(list)
+    for n in odd_range:
+        odd_0, odd_1, steps = odd_to_odd_step_counter(n,q)
+        step_dict[steps].append(odd_0)
 
-#Assume we can write out every odd number as: base_odd + even_offset*m
-#Exract out the even_offset from the list of all odds in a r (step) family
-r_m_dict = defaultdict(list)
-for r in step_dict:
-    r_odds = step_dict[r] #all odds in an r (step) family
+    step_dict = dict(sorted(step_dict.items())) #sort r (step) dict numerically
 
-    diff_list = [r_odds[i+1] - r_odds[i] for i in range(len(r_odds)-1)] #compute the difference between odds in an r (step) family
+    #Assume we can write out every odd number as: base_odd + even_offset*m
+    #Exract out the even_offset from the list of all odds in a r (step) family
+    r_m_dict = defaultdict(list)
+    for r in step_dict:
+        r_odds = step_dict[r] #all odds in an r (step) family
 
-    if diff_list:
-        base_odd = r_odds[0] #This is the base odd
-        even_offset = mode(diff_list) #Get the even_offset as the most likely difference between odds
+        diff_list = [r_odds[i+1] - r_odds[i] for i in range(len(r_odds)-1)] #compute the difference between odds in an r (step) family
 
-        #Add r, base_odd, and even_offset as lists to a dict
-        r_m_dict['r'].append(r)
-        r_m_dict['base_odd'].append(base_odd)
-        r_m_dict['even_offset'].append(even_offset)
+        if diff_list:
+            base_odd = r_odds[0] #This is the base odd
+            even_offset = mode(diff_list) #Get the even_offset as the most likely difference between odds
 
+            #Add r, base_odd, and even_offset as lists to a dict
+            r_m_dict['r'].append(r)
+            r_m_dict['base_odd'].append(base_odd)
+            r_m_dict['even_offset'].append(even_offset)
 
-print(r_m_dict)
+    return r_m_dict
 
-print(q,'n + 1')
-for i,r in enumerate(r_m_dict['r']):
+# print(q,'n + 1')
+# for i,r in enumerate(r_m_dict['r']):
     
-    r = r_m_dict['r'][i]
-    odd = r_m_dict['base_odd'][i]
-    m = r_m_dict['even_offset'][i]
+#     r = r_m_dict['r'][i]
+#     odd = r_m_dict['base_odd'][i]
+#     m = r_m_dict['even_offset'][i]
 
-    print('r:',r,'|', odd,'+', m,'m')
+#     print('r:',r,'|', odd,'+', m,'m')
 
-odd_list = r_m_dict['base_odd']
-odd_diff_list = [odd_list[i+1] - odd_list[i] for i in range(len(odd_list)-1)]
+# odd_list = r_m_dict['base_odd']
+# odd_diff_list = [odd_list[i+1] - odd_list[i] for i in range(len(odd_list)-1)]
 
-print(odd_diff_list)
+# print(odd_diff_list)
 
