6a70c931361fb1eac4eb4820c666a2a40d60c508
Joey <joseph.schmidt@utexas.edu>
2023-02-14 17:43:28 -0600

    Deleted CSV builder function

--- rm_dict_builder.py
+++ rm_dict_builder.py
@@ -2,7 +2,6 @@ from odd_to_odd_func import odd_to_odd_step_counter
 
 
 
-
 def rm_dict_builder(q, n_max):
     from statistics import mode
     from collections import defaultdict
@@ -38,36 +37,3 @@ def rm_dict_builder(q, n_max):
     rm_dict = dict(rm_dict) #Convert rm_dict list to dictionary
 
     return rm_dict, r_step_dict
-
-def r_step_dict_to_CSV(q, n_max):
-    import csv
-
-    rm_dict, r_step_dict = rm_dict_builder(q, n_max)
-
-    filename = str(q) + 'n_plus_1_r_step_data.csv'
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(rm_dict.keys())
-        writer.writerows(zip(*rm_dict.values()))
-
-
-
-
-def rm_dict_to_CSV(q, n_max):
-    import csv
-
-    rm_dict, r_step_dict = rm_dict_builder(q, n_max)
-
-    filename = str(q) + 'n_plus_1_rm_data.csv'
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(rm_dict.keys())
-        writer.writerows(zip(*rm_dict.values()))
-
-
-
-
-q = 7
-n_max = 1000003
-rm_dict_to_CSV(q, n_max)
-r_step_dict_to_CSV(q, n_max)
