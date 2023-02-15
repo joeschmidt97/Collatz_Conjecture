69cd834279dfb08044d081da5b7757ae270265f1
Joey <joseph.schmidt@utexas.edu>
2023-02-15 14:19:14 -0600

    Renamed directory and deleted original

--- DATA_qn_plus_1/CSV_rm_r_step_generator.py
+++
@@ -1,9 +0,0 @@
-import sys
-# sys.path.append('C:\\Users\\joesc\\git\\Collatz_Conjecture\\Collatz_Conjecture\\functions')
-sys.path.append('C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture/functions')
-import rm_to_CSV
-
-q = 9
-n_max = 103
-rm_to_CSV.WRITE_r_step_to_CSV(q, n_max)
-rm_to_CSV.WRITE_rm_dict_to_CSV(q, n_max)
