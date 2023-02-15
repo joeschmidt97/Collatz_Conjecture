cd7f6815b1a390b7295a88bd0d44b240fa4bf57e
Joey <joseph.schmidt@utexas.edu>
2023-02-14 18:45:35 -0600

    Added r-step families to CSV function

--- rm_to_CSV.py
+++ rm_to_CSV.py
@@ -3,20 +3,25 @@ from rm_dict_builder import rm_dict_builder
 
 
 def r_step_dict_to_CSV(q, n_max):
-
     rm_dict, r_step_dict = rm_dict_builder(q, n_max)
 
     filename = str(q) + 'n_plus_1_r_step_data.csv'
     with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(rm_dict.keys())
-        writer.writerows(zip(*rm_dict.values()))
+        writer = csv.writer(csvfile) # create a CSV writer object
 
+        # get the maximum length of the values in the dictionary
+        max_len = max(len(values) for values in r_step_dict.values())
+        col_labels = ['r'] + [f'm={i}' for i in range(max_len)] # create a list of column labels
+        writer.writerow(col_labels)         # write the header row with column names
+
+        # write the data rows with values and empty cells as needed
+        for key, values in r_step_dict.items():
+            row = [key] + values + [''] * (max_len - len(values))
+            writer.writerow(row)
 
 
 
 def rm_dict_to_CSV(q, n_max):
-    
     rm_dict, r_step_dict = rm_dict_builder(q, n_max)
 
     filename = str(q) + 'n_plus_1_rm_data.csv'
@@ -27,8 +32,7 @@ def rm_dict_to_CSV(q, n_max):
 
 
 
-
-q = 7
-n_max = 1000003
+q = 3
+n_max = 100003
 rm_dict_to_CSV(q, n_max)
 r_step_dict_to_CSV(q, n_max)
