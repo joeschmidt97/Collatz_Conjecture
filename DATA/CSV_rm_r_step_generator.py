10d38b39b7b71120e435d6715371231dec20d2de
Joey <joseph.schmidt@utexas.edu>
2023-02-15 14:37:28 -0600

    Deleted stored data

--- DATA/CSV_rm_r_step_generator.py
+++
@@ -1,9 +0,0 @@
-import sys
-# sys.path.append('C:\\Users\\joesc\\git\\Collatz_Conjecture\\Collatz_Conjecture\\functions')
-sys.path.append('C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture/functions')
-import rm_to_CSV
-
-q = 3
-n_max = 10000001
-rm_to_CSV.WRITE_r_step_to_CSV(q, n_max)
-rm_to_CSV.WRITE_rm_dict_to_CSV(q, n_max)
