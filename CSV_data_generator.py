22fc380ca3e70c778d0d3d8e70f32f7cdca33296
Joey <joseph.schmidt@utexas.edu>
2023-02-15 14:54:13 -0600

    Changed q value

--- CSV_data_generator.py
+++ CSV_data_generator.py
@@ -1,13 +1,10 @@
 import sys
+from FUNC_rm_to_CSV import WRITE_r_step_to_CSV, WRITE_rm_dict_to_CSV
 
 filepath_git = 'C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture'
-# sys.path.append('C:\\Users\\joesc\\git\\Collatz_Conjecture\\Collatz_Conjecture\\functions')
-sys.path.append(filepath_git + '/functions')
-from rm_to_CSV import WRITE_r_step_to_CSV
-
 filepath = filepath_git + '/DATA'
 
-q = 5
-n_max = 101
+q = 13
+n_max = 10000001
 WRITE_r_step_to_CSV(q, n_max, filepath)
-# rm_to_CSV.WRITE_rm_dict_to_CSV(q, n_max)
+WRITE_rm_dict_to_CSV(q, n_max, filepath)
