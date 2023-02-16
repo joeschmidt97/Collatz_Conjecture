1e382e0a1a564d56997fb8c62fedbe930d69a3f7
Joey <joseph.schmidt@utexas.edu>
2023-02-15 20:52:12 -0600

    Added printout to show which q values are being computed for

--- CSV_data_generator.py
+++ CSV_data_generator.py
@@ -5,6 +5,7 @@ filepath_git = 'C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture'
 filepath = filepath_git + '/DATA'
 
 q = 13
-n_max = 10000001
+n_max = 70000001
+print('Producing data for q=',q)
 WRITE_r_step_to_CSV(q, n_max, filepath)
 WRITE_rm_dict_to_CSV(q, n_max, filepath)
