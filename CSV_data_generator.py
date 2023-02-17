afcd7754f26fd5412021aa23e6530dfea9c10bf2
Joey <joseph.schmidt@utexas.edu>
2023-02-17 13:11:26 -0600

    Changed to new data generation for CSV

--- CSV_data_generator.py
+++ CSV_data_generator.py
@@ -1,11 +1,14 @@
-import sys
-from FUNC_rm_to_CSV import WRITE_r_step_to_CSV, WRITE_rm_dict_to_CSV
+from A_FUNC_rstep_data import WRITE_rstep_to_CSV
+from A_FUNC_rm_data import WRITE_rm_to_CSV
+# filepath_git = 'C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture'
+# filepath = filepath_git + '/DATA'
+#511
 
-filepath_git = 'C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture'
-filepath = filepath_git + '/DATA'
 
-q = 13
-n_max = 70000001
-print('Producing data for q=',q)
-WRITE_r_step_to_CSV(q, n_max, filepath)
-WRITE_rm_dict_to_CSV(q, n_max, filepath)
+
+q = 7
+n_max = 100001
+WRITE_rstep_to_CSV(q, n_max)
+WRITE_rm_to_CSV(q)
+# WRITE_r_step_to_CSV(q, n_max, filepath)
+# WRITE_rm_dict_to_CSV(q, n_max, filepath)
