120bf2883aba78ebb127c612b38c46bd0c752d2f
Joey <joseph.schmidt@utexas.edu>
2023-02-17 16:20:33 -0600

    Added for loops to generate lots of data for q values

--- CSV_data_generator.py
+++ CSV_data_generator.py
@@ -1,14 +1,21 @@
-from A_FUNC_rstep_data import WRITE_rstep_to_CSV
-from A_FUNC_rm_data import WRITE_rm_to_CSV
-# filepath_git = 'C:/Users/joesc/git/Collatz_Conjecture/Collatz_Conjecture'
-# filepath = filepath_git + '/DATA'
-#511
+from FUNC_rstep_data import WRITE_rstep_to_CSV
+from FUNC_rm_data import WRITE_rm_to_CSV
+
+def WRITE_rstep_rm_CSV(q, n_max):
+    WRITE_rstep_to_CSV(q, n_max)
+    WRITE_rm_to_CSV(q) 
+
+n_max = 1000000001
+# q_range = range(1,51,2)
+q_range = [11,13,17,19,23,25,27,29,33,35,37,39,41,43,45,47,49,51]
+for q in q_range:
+    print(q)
+    WRITE_rstep_rm_CSV(q, n_max)
 
 
 
-q = 7
-n_max = 100001
-WRITE_rstep_to_CSV(q, n_max)
-WRITE_rm_to_CSV(q)
-# WRITE_r_step_to_CSV(q, n_max, filepath)
-# WRITE_rm_dict_to_CSV(q, n_max, filepath)
+# x_range = [9,10]
+# for x in x_range:
+#     q = 2**x - 1
+#     print('q=',q)
+#     WRITE_rstep_rm_CSV(q, n_max)
