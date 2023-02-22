17680f3a3838691da5cffc7e4d8424931e1f7115
Joey <joseph.schmidt@utexas.edu>
2023-02-21 22:23:52 -0600

    Test data for q=501

--- CSV_data_generator.py
+++ CSV_data_generator.py
@@ -1,16 +1,22 @@
 from FUNC_rstep_data import WRITE_rstep_to_CSV
 from FUNC_rm_data import WRITE_rm_to_CSV
 
-def WRITE_rstep_rm_CSV(q, n_max):
-    WRITE_rstep_to_CSV(q, n_max)
+def WRITE_rstep_rm_CSV(q, n_min, n_max):
+    WRITE_rstep_to_CSV(q, n_min, n_max)
     WRITE_rm_to_CSV(q) 
 
-n_max = 1000000001
+# q = 51
+q = 501
+n_min = -1001
+n_max = 10001
+
+WRITE_rstep_rm_CSV(q, n_min, n_max)
+
+# n_max = 1000000001
 # q_range = range(1,51,2)
-q_range = [11,13,17,19,23,25,27,29,33,35,37,39,41,43,45,47,49,51]
-for q in q_range:
-    print(q)
-    WRITE_rstep_rm_CSV(q, n_max)
+# for q in q_range:
+#     print(q)
+#     WRITE_rstep_rm_CSV(q, n_max)
 
 
 
